9c1b5a63c3299e459d8751359b8b3777c042d8ad
Anoop Kurungadam <anoop@earthianslive.com>
2022-11-14 16:50:32 +0530

    feat: add config for frappe health site map appointment fields with dicom tags logfile

--- app.py
+++ app.py
@@ -15,57 +15,93 @@ import requests
 import signal
 import sys
 import time
-from logzero import logger
+from logzero import (
+    logger,
+    setup_logger,
+)
 from pynetdicom import AE, evt
 from pydicom.dataset import Dataset
+from pydicom.valuerep import DA, TM
 from pynetdicom.sop_class import (
     Verification,
     ModalityWorklistInformationFind,
-    PatientRootQueryRetrieveInformationModelFind,
     # TODO: ModalityPerformedProcedureStep
 )
 
-CONFIG_PATH = r'config.json'
-config = dict()  # cache
-debug = False
+CONFIG_PATH = "config.json"
+config = dict()
+
+appointment_worklist_map = {
+        "name": "", # "ScheduledProcedureStepID", # ?
+        "status": "", # "ScheduledProcedureStepStatus", # ?
+        "patient": "PatientID",
+        "patient_name": "PatientName",
+        "appointment_date": "ScheduledStudyStartDate",
+        "appointment_time": "ScheduledStudyStartTime",
+        "procedure_template": "ScheduledProcedureStepID",
+        "practitioner_name": "ScheduledPerformingPhysicianName",
+        "referring_practitioner": "RequestingPhysician",
+        "service_unit": "Modality",
+        "": "ScheduledStationAETitle",
+        "": "ScheduledProcedureStepLocation",
+        "": "ScheduledProcedureStepDescription",
+        "": "PreMedication",
+}
 
 
 def handle_echo(event):
     """
-    Handle a C-ECHO request event.
-    https://pydicom.github.io/pynetdicom/stable/reference/generated/pynetdicom._handlers.doc_handle_echo.html
+        Handle a C-ECHO request event.
+        https://pydicom.github.io/pynetdicom/stable/reference/generated/pynetdicom._handlers.doc_handle_echo.html
     """
 
-    if debug:
-        logger.info(f"C-ECHO: {vars(event)}")
-
-    return 0x0000  # Success
+    logger.info(f"C-ECHO: {vars(event)}")
+    return 0x0000 # Success
 
 
 def handle_find(event):
     """
-    Handle a C-FIND request event.
-    https://pydicom.github.io/pynetdicom/stable/reference/generated/pynetdicom._handlers.doc_handle_find.html
+        Handle a C-FIND request event.
+        https://pydicom.github.io/pynetdicom/stable/reference/generated/pynetdicom._handlers.doc_handle_find.html
     """
-    ds = event.identifier
-    if debug:
-        logger.debug(f"C-FIND: {vars(event)}")
 
+    logger.info(f"C-FIND: {vars(event)}")
+
+    ds = event.identifier
+    if "QueryRetrieveLevel" not in ds:
+        yield 0xC000, None
+        return
+
+    worklist = []
     filters = {}
 
-    if ds.QueryRetrieveLevel == 'PATIENT':
-        if 'PatientName' in ds:
-            if ds.PatientName not in ['*', '', '?']:
-                filters.update({'patient': str(ds.PatientName)})
-                if debug:
-                    logger.info(f"ds.PatientName not in ['*', '', '?']")
+    if ds.QueryRetrieveLevel == "PATIENT":
+        if "PatientName" in ds:
+            if ds.PatientName not in ["*", "", "?"]:
+                filters.update({"patient_name": str(ds.PatientName)})
 
-        if 'ScheduledStudyStartDate' in ds:
-            if ds.ScheduledStudyStartDate not in ['*', '', '?']:
-                filters.update({'appointment_date': str(ds.ScheduledStudyStartDate)})
-                if debug:
-                    logger.info(f"ds.ScheduledStudyStartDate not in ['*', '', '?']")
-        # TODO: Build further filters
+        if "PatientID" in ds and ds.PatientID:
+            filters.update({"patient": str(ds.PatientID)})
+
+        if "ScheduledStudyStartDate" in ds and ds.ScheduledStudyStartDate:
+            filters.update({"appointment_date": DA(ds.ScheduledStudyStartDate).isoformat()})
+
+        # FIXME
+        # if "ScheduledStudyStartTime" and ds.ScheduledStudyStartTime:
+        # 		print(TM(ds.ScheduledStudyStartTime).isoformat())
+        # 		filters.update({"appointment_time": TM(ds.ScheduledStudyStartTime).isoformat()})
+
+        if "RequestingPhysician" in ds and ds.RequestingPhysician:
+            filters.update({"practitioner": str(ds.RequestingPhysician)})
+
+        if "ScheduledPerformingPhysicianName" in ds and ds.ScheduledPerformingPhysicianName:
+            filters.update({"practitioner": str(ds.ScheduledPerformingPhysicianName)})
+
+        if "ScheduledStationAETitle" in ds and ds.ScheduledStationAETitle:
+            filters.update({"ae_title": str(ds.ScheduledStationAETitle)})
+
+        if "Modality" in ds and ds.Modality:
+            filters.update({"service_unit": str(ds.Modality)})
 
         worklist = get_appointments(filters)
 
@@ -74,53 +110,70 @@ def handle_find(event):
         yield 0xC000, None
         return
 
+    logger.info(f"Sending {len(worklist)} worklist items")
+
     for workitem in worklist:
+
         # Check if C-CANCEL has been received
         if event.is_cancelled:
-            if debug:
-                logger.info(f"Event Cancelled {vars(event)}")
+            logger.info(f"Event Cancelled {vars(event)}")
 
-            yield (0xFE00, None)  # Matching terminated due to Cancel request
+            yield (0xFE00, None) # Matching terminated due to Cancel request
             return
 
         identifier = Dataset()
-        identifier.QueryRetrieveLevel = ds.QueryRetrieveLevel
-        identifier.PatientName = workitem.get('patient')
-        identifier.ScheduledStudyStartDate = workitem.get('appointment_date')
+        setattr(identifier, "QueryRetrieveLevel", ds.QueryRetrieveLevel)
 
-        if debug:
-            logger.info(f"Yeilding {vars(identifier)}")
+        for field, dicom_tag in appointment_worklist_map.items():
+
+            if workitem.get(field) and dicom_tag:
+                if field == "appointment_date":
+                    setattr(identifier, dicom_tag, DA.fromisoformat(workitem.get("appointment_date")).strftime("%Y%m%d"))
+
+                elif field == "appointment_time":
+                    continue
+                    # FIXME: skipping appointment_time
+                    # identifier[dicom_tag] = TM.fromisoformat(workitem.get("appointment_time").strftime("%H%M%S"))
+
+                else:
+                    setattr(identifier, dicom_tag, workitem.get(field))
+
+        # TODO: validate prepared dataset?
+        # https://pydicom.github.io/pydicom/dev/reference/generated/pydicom.sequence.validate_dataset.html
+
+        logger.info(f"Yield {vars(identifier)}")
         # Pending
         # Matches are continuing: current match is supplied and any Optional Keys were supported in the same manner as Required Keys
         yield (0xFF00, identifier)
 
+    logger.info(f"Worklist send complete")
+
 
 def get_appointments(filters):
-    fields = ["name", "patient", "appointment_date", "status"]
+
+    fields = [field for field in list(appointment_worklist_map.keys()) if field]
     config = get_config()
-    url = f"{config.get('url')}/api/resource/Patient Appointment"
+    host_name = f"{config.get('host_name')}/api/resource/Patient Appointment"
 
     headers = {
-        'Authorization': f"token {config.get('api_key')}:{config.get('api_secret')}",
-        'Accept': 'application/json',
+        "Authorization": f"token {config.get('api_key')}:{config.get('api_secret')}",
+        "Accept": "application/json",
     }
 
     data = {
-        'fields': json.dumps(fields),
-        'filters': json.dumps(filters),
+        "fields": json.dumps(fields),
+        "filters": json.dumps(filters),
     }
 
-    if debug:
-        logger.info(f"Request URL: {url}\nHeaders: {headers}\nData: {data}")
+    logger.info(f"Request URL: {host_name}\nHeaders: {headers}\nData: {data}")
 
-    response = requests.request("GET", url, headers=headers, data=data)
+    response = requests.request("GET", host_name, headers=headers, data=data)
     appointments = json.loads(response._content)
 
-    if debug:
-        logger.info(f"Response: {appointments}")
+    logger.info(f"Response: {appointments}")
 
-    if appointments and appointments.get('data'):
-        return appointments.get('data')
+    if appointments and appointments.get("data"):
+        return appointments.get("data")
     else:
         return []
 
@@ -135,8 +188,8 @@ def start_mwl_scp(args):
     https://pydicom.github.io/pynetdicom/stable/reference/generated/pynetdicom.ae.ApplicationEntity.html
     """
 
-    title, address, port = args.title, args.address, int(args.port)
-    logger.info(f"Starting MWL SCP {title} on {address}:{port}")
+    title, port = args.title, int(args.port)
+    logger.info(f"Starting MWL SCP {title} on {port}")
 
     handlers = [
         (evt.EVT_C_ECHO, handle_echo),
@@ -149,11 +202,9 @@ def start_mwl_scp(args):
     # Add the supported presentation context
     ae.add_supported_context(Verification)
     ae.add_supported_context(ModalityWorklistInformationFind)
-    ae.add_supported_context(PatientRootQueryRetrieveInformationModelFind)
 
-    if debug:
-        for sc in ae.supported_contexts:
-            logger.info(sc)
+    for sc in ae.supported_contexts:
+        logger.info(sc)
 
     # Start listening for incoming association requests
     ae.start_server(("127.0.0.1", port), evt_handlers=handlers)
@@ -166,10 +217,9 @@ def stop_mwl_scp(signal, frame):
     logger.info(f"Received SIGINT, shutting down MWL SCP")
 
     # Wait for existing associations to finish
-    while (True):
+    while(True):
         if ae.active_associations:
-            if debug:
-                logger.info(f"{ae.active_associations} ative Associations, waiting")
+            logger.info(f"{ae.active_associations} active Associations, waiting")
             time.sleep(1)
         else:
             break
@@ -179,37 +229,27 @@ def stop_mwl_scp(signal, frame):
 
 
 def get_config():
-    if config:
-        return config
-    else:
-        with open(CONFIG_PATH) as f:
-            config.update(json.load(f))
+    if not config:
+        with open(CONFIG_PATH) as f: config.update(json.load(f))
+
     return config
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        prog="mwl_scp",
-        description="Adapter to support DIMSE C-FIND and C-ECHO for modalities interacting with Frappe Health",
+        prog="RIS-Bridge",
+        description="Bridge to support DIMSE C-FIND and C-ECHO for modalities interacting with Frappe Health",
         epilog="Experimental, not intended for Production use"
     )
 
     # Required positional arguments
     parser.add_argument("title", help="AE Title for the MWL SCP, required")
-    parser.add_argument("-a", "--address", dest="address", default='0.0.0.0',
-                        help="Address for MWL SCP to listen, default 0.0.0.0")
-    parser.add_argument("-p", "--port", dest="port", default=8042, help="Port for MWL SCP to listen, default 104")
-
-    # TODO: handle verbosity
-    parser.add_argument(
-        "-d",
-        "--debug",
-        action="store_true",
-        help="Log debug information")
+    parser.add_argument("-p", "--port", dest="port", default=104, help="Port for MWL SCP to listen, default 104")
 
     signal.signal(signal.SIGINT, stop_mwl_scp)
 
+    logger = setup_logger("logger", logfile="logs/mwl-scp.log", maxBytes=1000000, backupCount=10)
+
     args = parser.parse_args()
-    debug = True if args.debug else False
 
     start_mwl_scp(args)
